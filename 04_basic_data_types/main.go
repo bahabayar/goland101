e5a20becc38a8b8e6b09d28efc00de5d51f04ce8
Baha <bahabayar@hotmail.com>
2022-07-11 01:11:03 +0300

    zero values and multiple declaration for variables

--- 04_basic_data_types/main.go
+++ 04_basic_data_types/main.go
@@ -7,11 +7,18 @@ func main() {
 	var age int = 23
 	var isMarried bool = false
 
+	var weight float64 = 75.64
+	var height int = 180
+
 	fmt.Println(name)
 	fmt.Println(age)
 	fmt.Println(isMarried)
+	fmt.Println(weight)
+	fmt.Println(height)
 
 	fmt.Printf("%T\n", name)
 	fmt.Printf("%T\n", age)
 	fmt.Printf("%T\n", isMarried)
+	fmt.Printf("%T\n", weight)
+	fmt.Printf("%T\n", height)
 }
---
+++ 05-Zero_Values_Creating_Multiple_Variables/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var (
+	//	name           = "Baha"
+	//	age       int  = 23
+	//	isMarried bool = false
+	//
+	//	weight float64 = 75.64
+	//	height int     = 180
+	//)
+
+	//var name, age, isMarried, weight, height = "Baha", 23, false, 75.64, 180
+	//
+	//name, age, isMarried, weight, height := "Baha", 23, false, 75.64, 180
+
+	var name string
+	var age int
+	var isMarried bool
+
+	var height float64
+	var birthMonth byte
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(height)
+	fmt.Println(birthMonth)
+}
