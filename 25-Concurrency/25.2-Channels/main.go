4e9e7f5eed0744c24b7959b4bff070eb5f6635cd
Baha <bahabayar@hotmail.com>
2022-08-17 00:16:42 +0300

    wait group added

--- 25-Concurrency/25.2-Channels/main.go
+++ 25-Concurrency/25.2-Channels/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 //func firstRoutine(channel chan bool) {
 //	fmt.Println("My first Routine")
@@ -32,7 +37,27 @@ func sumOnlyEvenNumbers(evenChannel chan int, number int) {
 
 }
 
+func printY() {
+	for i := 0; i < 100; i++ {
+		fmt.Print("Y")
+	}
+
+}
+func printX() {
+
+	for i := 0; i < 100; i++ {
+		fmt.Print("X")
+	}
+	wg.Done() /*Routine is done*/
+}
+
 func main() {
+
+	wg.Add(1) /*This method used for to tell wait for other go routines for main routine*/
+	go printX()
+	wg.Wait()
+	printY()
+	//time.Sleep(time.Second)
 	//var ch chan int
 	//if ch == nil {
 	//	ch = make(chan int)
@@ -43,12 +68,12 @@ func main() {
 	//<-end
 	//fmt.Println("The End")
 
-	oChannel := make(chan int)
-	eChannel := make(chan int)
-
-	go sumOnlyOddNumbers(oChannel, 10)
-	go sumOnlyEvenNumbers(eChannel, 10)
-
-	totalOdds, totalEvens := <-oChannel, <-eChannel
-	fmt.Println("Odds Numbers + EvenNumbers = ", totalOdds+totalEvens)
+	//oChannel := make(chan int)
+	//eChannel := make(chan int)
+	//
+	//go sumOnlyOddNumbers(oChannel, 10)
+	//go sumOnlyEvenNumbers(eChannel, 10)
+	//
+	//totalOdds, totalEvens := <-oChannel, <-eChannel
+	//fmt.Println("Odds Numbers + EvenNumbers = ", totalOdds+totalEvens)
 }
